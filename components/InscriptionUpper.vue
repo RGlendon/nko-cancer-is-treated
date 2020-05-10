c26dc33553cf5284f7435223266259ab790fa257
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2020-05-10 15:14:18 +0400

    my current changes

---
+++ components/InscriptionUpper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="InscriptionUpper">
+    <h3 class="InscriptionUpper__text">И В ОТЛИЧИЕ ОТ РАКА, #ЭТОНЕЛЕЧИТСЯ</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+.InscriptionUpper {
+  width: 100%;
+  min-height: 86px;
+  background-color: #613A93;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.InscriptionUpper__text {
+  color: #fff;
+}
+
+</style>
