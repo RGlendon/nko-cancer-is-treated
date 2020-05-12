e39983d5e2cb3682a3000c1ba4af61234bdf5d84
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2020-05-12 22:58:18 +0400

    added PageNumber

--- components/UI/PageNumber.vue
+++ components/UI/PageNumber.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-  <!--  TODO: button more articles. Responsible - Alexander-->
-  </div>
+  <ul class="page-numbers">
+    <li class="page-number page-number_active">{{number}}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'PageNumber.vue',
+  props: ['number'],
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.page-numbers {
+  max-width: 480px;
+  margin: 300px auto;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+
+.page-number {
+  width: 58px;
+  height: 58px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+}
+.page-numbers :last-child {
+  margin-right: 0;
+}
+
+.page-number_active {
+  background-color: #F4F4F4;
+}
+
+</style>
