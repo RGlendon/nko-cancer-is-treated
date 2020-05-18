e6d195dd185a474c8daa64e39bd5f7f8f1078b1f
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2020-05-18 23:29:02 +0400

    corrected corrected git commit -m corrected

---
+++ components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="['container', className]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    className: String,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1440px;
+  padding: 0px 60px;
+  margin: 0 auto;
+  width: 100%;
+}
+</style>
